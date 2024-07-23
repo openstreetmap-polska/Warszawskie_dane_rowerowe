ce662068a37ec84dc857b138629b8facd7c82c75
Filip Czaplicki <github@starsep.com>
2024-07-23 23:10:11 +0200

    h3

--- osm_diff.py
+++ osm_diff.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-from math import radians, sin, atan2, sqrt, cos, ceil
-from dataclasses import dataclass
+import h3
 import httpx
 from tqdm import tqdm
 
@@ -15,10 +14,10 @@ dataDirectory = Path("rowery_wawa")
 shapefilePath = dataDirectory / "rowery.shp"
 outputDirectory = Path("osm_diffs")
 OVERPASS_URL = "https://overpass-api.de/api/interpreter"  # "http://localhost:12345/api/interpreter"
-LINE_STEP_METRES = 1
-POINT_DISTANCE_METRES = 10
-MISSING_COUNT_THRESHOLD = 20
+MISSING_COUNT_THRESHOLD = 10
 MISSING_COUNT_PERCENTAGE_THRESHOLD = 0.2
+H3_RESOLUTION = 11
+NEIGHBOURHOOD_SIZE = 1
 
 
 log_duration = log_durations(lambda msg: print("⌛ " + msg))
@@ -49,85 +48,27 @@ def getOSMDataFromOverpass():
     return geojson.loads(response.text)["elements"]
 
 
-@dataclass
-class GeoPoint:
-    lat: float
-    lon: float
-
-    @staticmethod
-    def fromTuple(tup):
-        return GeoPoint(tup[1], tup[0])
-
-
-warsawCenter = GeoPoint(lat=52.2319581, lon=21.0067249)
-
-
-def geoDistance(point: GeoPoint, other: GeoPoint) -> int:
-    R = 6373.0
-    lon1, lat1, lon2, lat2 = map(radians, [point.lon, point.lat, other.lon, other.lat])
-
-    deltaLon = lon2 - lon1
-    deltaLat = lat2 - lat1
-    a = (sin(deltaLat / 2)) ** 2 + cos(lat1) * cos(lat2) * (sin(deltaLon / 2)) ** 2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    return int(R * c * 1000)
-
-
-DictPoint = dict[int, list[GeoPoint]]
-
-
-def pointsToDictPoint(points: list[GeoPoint]) -> DictPoint:
-    result = dict()
-    for point in points:
-        distance = geoDistance(point, warsawCenter)
-        if distance not in result:
-            result[distance] = []
-        result[distance].append(point)
-    return result
-
-
-def processLineIntoDictPoint(line: list[tuple[float, float]]) -> DictPoint:
-    points = []
+def processLineIntoH3Set(line: list[tuple[float, float]], result: set[str]) -> set[str]:
     for pointA, pointB in zip(line[:-1], line[1:]):
-        latDelta = pointB[1] - pointA[1]
-        lonDelta = pointB[0] - pointA[0]
-        distance = geoDistance(GeoPoint.fromTuple(pointA), GeoPoint.fromTuple(pointB))
-        stepsCount = int(ceil(distance / LINE_STEP_METRES))
-        if stepsCount == 0:
-            continue
-        stepLat = latDelta / stepsCount
-        stepLon = lonDelta / stepsCount
-        for i in range(stepsCount):
-            points.append(
-                GeoPoint(lon=pointA[0] + stepLon * i, lat=pointA[1] + stepLat * i)
-            )
-    result = pointsToDictPoint(points)
-    return result
-
-
-def mergeDictPoints(dictPoints: list[DictPoint]) -> DictPoint:
-    result = dict()
-    for dictPoint in dictPoints:
-        for distance, pointsInDistance in dictPoint.items():
-            if distance not in result:
-                result[distance] = []
-            result[distance].extend(pointsInDistance)
+        start = h3.geo_to_h3(pointA[1], pointA[0], H3_RESOLUTION)
+        end = h3.geo_to_h3(pointB[1], pointB[0], H3_RESOLUTION)
+        for point in h3.h3_line(start, end):
+            result.update(h3.k_ring(point, NEIGHBOURHOOD_SIZE))
     return result
 
 
 @log_duration
-def processOSMDataIntoDictPoint(osmData) -> DictPoint:
-    dictPoints = []
+def processOSMDataIntoH3Set(osmData) -> set[str]:
+    result = set()
     for element in osmData:
         if element["geometry"]["type"] != "LineString":
             print(f'Unsupported geometry type {element["geometry"]["type"]}')
             continue
         coords = element["geometry"]["coordinates"]
-        dictPoints.append(processLineIntoDictPoint(coords))
-    return mergeDictPoints(dictPoints)
+        result = processLineIntoH3Set(coords, result)
+    return result
 
 
-@log_duration
 def outputMissingFeaturesGeojson(name: str, missingFeatures):
     nameSlugified = slugify(name, lowercase=False)
     outputFile = outputDirectory / (nameSlugified + ".geojson")
@@ -141,40 +82,20 @@ def outputMissingFeaturesGeojson(name: str, missingFeatures):
         )
 
 
-@log_duration
-def processDistrict(district, districtFeatures, osmDictPoint):
+def processDistrict(district, districtFeatures, osmH3Set: set[str]):
     missingFeatures = []
-    for feature in tqdm(districtFeatures):
+    for feature in districtFeatures:
+        featureH3Set = set()
         if feature["geometry"]["type"] == "LineString":
-            warsawDataPoints = processLineIntoDictPoint(
-                feature["geometry"]["coordinates"]
-            )
+            featureH3Set = processLineIntoH3Set(feature["geometry"]["coordinates"], featureH3Set)
         elif feature["geometry"]["type"] == "MultiLineString":
-            warsawDataPoints = mergeDictPoints(
-                [
-                    processLineIntoDictPoint(line)
-                    for line in feature["geometry"]["coordinates"]
-                ]
-            )
+            for line in feature["geometry"]["coordinates"]:
+                featureH3Set = processLineIntoH3Set(line, featureH3Set)
         else:
             print(f'Unsupported geometry type {feature["geometry"]["type"]}')
             continue
-        missingCount = 0
-        count = 0
-        for distanceFromCenter, pointsInDist in warsawDataPoints.items():
-            count += len(pointsInDist)
-            for point in pointsInDist:
-                found = False
-                for dist in range(
-                    distanceFromCenter - POINT_DISTANCE_METRES,
-                    distanceFromCenter + POINT_DISTANCE_METRES + 1,
-                ):
-                    for osmPoint in osmDictPoint.get(dist, []):
-                        if geoDistance(point, osmPoint) < POINT_DISTANCE_METRES:
-                            found = True
-                            break
-                if not found:
-                    missingCount += 1
+        count = len(featureH3Set)
+        missingCount = count - len(osmH3Set & featureH3Set)
         if (
             missingCount >= MISSING_COUNT_THRESHOLD
             or missingCount / count > MISSING_COUNT_PERCENTAGE_THRESHOLD
@@ -184,7 +105,8 @@ def processDistrict(district, districtFeatures, osmDictPoint):
     outputMissingFeaturesGeojson(district, missingFeatures)
 
 
-def generateOSMDiff(warsawData, osmDictPoint):
+@log_duration
+def generateOSMDiff(warsawData, osmH3Set: set[str]):
     warsawDistricts = [
         "Bemowo",
         "Białołęka",
@@ -252,15 +174,15 @@ def generateOSMDiff(warsawData, osmDictPoint):
             for feature in analyzedFeatures
             if feature["properties"]["DZIELNICA"] == areaName
         ]
-        processDistrict(areaName, districtFeatures, osmDictPoint)
+        processDistrict(areaName, districtFeatures, osmH3Set)
 
 
 def main():
     outputDirectory.mkdir(exist_ok=True)
     warsawData = openDataGeojson()
     osmData = getOSMDataFromOverpass()
-    osmDictPoint = processOSMDataIntoDictPoint(osmData)
-    generateOSMDiff(warsawData, osmDictPoint)
+    h3Set = processOSMDataIntoH3Set(osmData)
+    generateOSMDiff(warsawData, h3Set)
 
 
 if __name__ == "__main__":
